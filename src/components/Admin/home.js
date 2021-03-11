49bfdedcbf291f99e31d22abf1ecd18cf20b2c12
Peeyush Goyal <smartrohan1514@gmail.com>
2021-03-11 23:04:52 +0530

    Completed UI Home Page

--- src/components/Admin/home.js
+++ src/components/Admin/home.js
@@ -7,8 +7,33 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 import PersonIcon from "@material-ui/icons/Person";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.info.dark,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,6 +44,9 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
+  },
+  table: {
+    minWidth: 450,
   },
 }));
 
@@ -91,25 +119,44 @@ let AdminHome = () => {
                   </p>
                 </div>
               </div>
-              <div>
-                <table>
-                  <th>
-                    <td>Date</td>
-                    <td>Check-In</td>
-                    <td>Check-Out</td>
-                  </th>
-                  {x.Time.map((time, i) => {
-                    if (i % 2 === 0) {
-                      return (
-                        <tr>
-                          <td>{time.substr(0, 10)}</td>
-                          <td>{time.substr(11)}</td>
-                          <td>{x.Time[i + 1].substr(11)}</td>
-                        </tr>
-                      );
-                    }
-                  })}
-                </table>
+              <div className="home__userTable">
+                <TableContainer component={Paper}>
+                  <Table
+                    className={classes.table}
+                    aria-label="customized table"
+                  >
+                    <TableHead>
+                      <TableRow>
+                        <StyledTableCell>Date</StyledTableCell>
+                        <StyledTableCell align="right">
+                          Check-In
+                        </StyledTableCell>
+                        <StyledTableCell align="right">
+                          Check-Out
+                        </StyledTableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {x.Time.map((time, i) => {
+                        if (i % 2 === 0) {
+                          return (
+                            <StyledTableRow key={time.substr(0, 10)}>
+                              <StyledTableCell component="th" scope="row">
+                                {time.substr(0, 10)}
+                              </StyledTableCell>
+                              <StyledTableCell align="right">
+                                {time.substr(11)}
+                              </StyledTableCell>
+                              <StyledTableCell align="right">
+                                {x.Time[i + 1].substr(11)}
+                              </StyledTableCell>
+                            </StyledTableRow>
+                          );
+                        }
+                      })}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
               </div>
             </div>
           );
