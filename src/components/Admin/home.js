f86b5bb6a1c45557fa2ac676ea7e0c8ebd77e2e5
Peeyush Goyal <smartrohan1514@gmail.com>
2021-03-12 19:51:46 +0530

    UI Changes 1

--- src/components/Admin/home.js
+++ src/components/Admin/home.js
@@ -16,6 +16,12 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Row, Col, Image } from "reactstrap";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -37,7 +43,8 @@ const StyledTableRow = withStyles((theme) => ({
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    // flexGrow: 1,
+    width: "100%",
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -48,12 +55,27 @@ const useStyles = makeStyles((theme) => ({
   table: {
     minWidth: 450,
   },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: "33.33%",
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
 }));
 
 let AdminHome = () => {
   const [data, setData] = useState([]);
 
   const classes = useStyles();
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
   const fetchData = async () => {
     const response = db.collection("User_Data");
@@ -73,7 +95,7 @@ let AdminHome = () => {
     fetchData();
   }, []);
   return (
-    <div>
+    <div style={{ backgroundColor: "grey" }}>
       <div className="home__Title">
         <AppBar position="static">
           <Toolbar>
@@ -94,73 +116,90 @@ let AdminHome = () => {
           </Toolbar>
         </AppBar>
       </div>
-      <div className="home__cardDisplay">
-        {data.map((x, i) => {
-          console.log(x.Time);
-          return (
-            <div key={x.User_ID} className="home__userDetails">
-              <div className="home__userPersonalDetails">
-                <div className="home__userImgDiv">
-                  <img
-                    src={x.Image}
-                    alt="person-im"
-                    className="home__userImg"
-                  />
-                </div>
-                <div className="home__userTextDetails">
-                  <p>
-                    <b>User Id:</b> {x.User_ID}
-                  </p>
-                  <p>
-                    <b>Name:</b> {x.Name}
-                  </p>
-                  <p>
-                    <b>Created on:</b> {x.Date}
-                  </p>
-                </div>
-              </div>
-              <div className="home__userTable">
-                <TableContainer component={Paper}>
-                  <Table
-                    className={classes.table}
-                    aria-label="customized table"
+      <div className="row">
+        <div className="col-10 offset-1">
+          {data.map((x, i) => {
+            console.log(x.Time);
+            return (
+              <div
+                key={x.User_ID}
+                className="home__userDetailsBack col-8 offset-2 mt-5 mb-5"
+              >
+                <Accordion
+                  expanded={expanded === `panel${i + 1}`}
+                  onChange={handleChange(`panel${i + 1}`)}
+                >
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
                   >
-                    <TableHead>
-                      <TableRow>
-                        <StyledTableCell>Date</StyledTableCell>
-                        <StyledTableCell align="right">
-                          Check-In
-                        </StyledTableCell>
-                        <StyledTableCell align="right">
-                          Check-Out
-                        </StyledTableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {x.Time.map((time, i) => {
-                        if (i % 2 === 0) {
-                          return (
-                            <StyledTableRow key={time.substr(0, 10)}>
-                              <StyledTableCell component="th" scope="row">
-                                {time.substr(0, 10)}
-                              </StyledTableCell>
-                              <StyledTableCell align="right">
-                                {time.substr(11)}
-                              </StyledTableCell>
-                              <StyledTableCell align="right">
-                                {x.Time[i + 1].substr(11)}
-                              </StyledTableCell>
-                            </StyledTableRow>
-                          );
-                        }
-                      })}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
+                    <Typography className={classes.heading}>
+                      {x.Name}
+                    </Typography>
+                    <Typography
+                      className={classes.secondaryHeading}
+                    ></Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <div className="row">
+                      <div className="col-4">
+                        <img
+                          src={x.Image}
+                          alt="person-im"
+                          className="home__userImg"
+                        />
+                      </div>
+
+                      <div className="col-8">
+                        <TableContainer component={Paper}>
+                          <Table
+                            className={classes.table}
+                            aria-label="customized table"
+                          >
+                            <TableHead>
+                              <TableRow>
+                                <StyledTableCell>Date</StyledTableCell>
+                                <StyledTableCell align="right">
+                                  Check-In
+                                </StyledTableCell>
+                                <StyledTableCell align="right">
+                                  Check-Out
+                                </StyledTableCell>
+                              </TableRow>
+                            </TableHead>
+                            <TableBody>
+                              {x.Time.map((time, i) => {
+                                if (i % 2 === 0) {
+                                  return (
+                                    <StyledTableRow key={time.substr(0, 10)}>
+                                      <StyledTableCell
+                                        component="th"
+                                        scope="row"
+                                      >
+                                        {time.substr(0, 10)}
+                                      </StyledTableCell>
+                                      <StyledTableCell align="right">
+                                        {time.substr(11)}
+                                      </StyledTableCell>
+                                      <StyledTableCell align="right">
+                                        {x.Time[i + 1].substr(11)}
+                                      </StyledTableCell>
+                                    </StyledTableRow>
+                                  );
+                                }
+                              })}
+                            </TableBody>
+                          </Table>
+                        </TableContainer>
+                      </div>
+                    </div>
+                  </AccordionDetails>
+                </Accordion>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
