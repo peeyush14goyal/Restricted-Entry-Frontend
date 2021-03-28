2f60bad1b7365a93f73d2b1ef1c201a8589eb595
Razaul Hasan Ansari <razaulhasanansari2000@gmail.com>
2021-03-28 14:44:35 +0530

    Some changes got undone did them again

--- src/components/OneUser/OneUser.js
+++ src/components/OneUser/OneUser.js
@@ -35,16 +35,12 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "row",
   },
   grey: {
-<<<<<<< HEAD
-    color: "rgb(157, 148, 173)",
-=======
     color: "#dbd6f8",
   },
   grey1: {
     color: "#dbd6f8",
     fontSize: "20px",
     marginTop: "15px",
->>>>>>> 016ac5dc4b2f2598e5d20a09285c5def51ef6478
   },
 }));
 
@@ -85,11 +81,7 @@ export default function OneUser({ data }) {
                           <Typography
                             variant="body2"
                             component="p"
-<<<<<<< HEAD
-                            className={classes.grey}
-=======
                             className={classes.grey1}
->>>>>>> 016ac5dc4b2f2598e5d20a09285c5def51ef6478
                           >
                             {x.Time.length / 2 > 10
                               ? x.Time.length / 2 + " visits"
